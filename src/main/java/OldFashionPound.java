b9f45d3700f5aaa5842e9c90b45c086c56fc03fa
Jesus Valenti <jesus.valenti@hotmail.it>
2022-07-11 13:27:33 +0300

    Added makeDiff()

--- src/main/java/OldFashionPound.java
+++ src/main/java/OldFashionPound.java
@@ -13,12 +13,22 @@ public class OldFashionPound {
 
     Pound mySumResult = makeSum(myFirstPound, secondPound);
     System.out.println(mySumResult);
+    Pound myDiffResult = makeDiff(myFirstPound, secondPound);
+    System.out.println(myDiffResult);
 
   }
 
   public static Pound makeSum(Pound firstPound, Pound secondPound) {
 
     int myTotal = firstPound.getTotalValue() + secondPound.getTotalValue();
+    System.out.println(myTotal);
+    return new Pound(myTotal);
+
+  }
+
+  public static Pound makeDiff(Pound firstPound, Pound secondPound) {
+
+    int myTotal = firstPound.getTotalValue() - secondPound.getTotalValue();
     System.out.println(myTotal);
     return new Pound(myTotal);
 
